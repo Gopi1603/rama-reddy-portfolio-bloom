0ebbe7c18e80abe40f784ef503a4b83248d3d437
GOPI MANDEPUDI <132547360+Gopi1603@users.noreply.github.com>
2025-06-19 12:10:37 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,8 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen bg-background">
+   <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white">
+     
       <Header />
       <main className="container mx-auto px-6 lg:px-8 py-12 lg:py-16">
         {children}
